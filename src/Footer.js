8582f0a259c7008a7b20c5d4c4e3d53ef240e1da
belek <belektolubaev@gmail.com>
2019-01-28 19:15:16 +0600

    добавление футер

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import {Row, Col } from 'reactstrap';
 export default class Footer extends React.Component {
     render() {
         return (
-            <Row style={{ backgroundColor: "black", width: "100%" }}>
+            <Row style={{ backgroundColor: "green", width: "100%" }}>
                 <Col xs={12}>
                     <p className="text-white mt-2">Адрес: {this.props.addres}</p>
                     <p className="text-white">Телефон: {this.props.phone}</p>
